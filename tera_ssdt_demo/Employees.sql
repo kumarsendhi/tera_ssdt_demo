b46257d341bbfa8762734baae8ebf16ed1b5b1e9
Kumar Krishnamurthy <kumar.sendhi@gmail.com>
2019-05-10 06:28:34 +0000

    address column added

--- tera_ssdt_demo/Employees.sql
+++ tera_ssdt_demo/Employees.sql
@@ -2,5 +2,6 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Employee_Name] NVARCHAR(MAX) NOT NULL, 
-    [Employee_DOB] DATE NOT NULL
+    [Employee_DOB] DATE NOT NULL, 
+    [Address] NVARCHAR(MAX) NOT NULL
 )
